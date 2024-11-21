f7f3d2251a362c086811fe71c1c3e7e0f84bc919
Tsugu <masatsugu_yamada@mail.toyota.co.jp>
2024-11-22 04:14:42 +0900

    #5 fix シード作成

--- backend/db/seeds/007-picture.js
+++ backend/db/seeds/007-picture.js
@@ -4,18 +4,18 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('picture').del()
-  await knex('picture').insert([
-    {id: 1, wc_desc_id:5, path_name:"../picture/female-1.png"},
-    {id: 2, wc_desc_id:5, path_name:"../picture/female-2.jpeg"},
-    {id: 3, wc_desc_id:9, path_name:"../picture/female-3.jpg"},
-    {id: 4, wc_desc_id:1, path_name:"../picture/male-4.png"},
-    {id: 5, wc_desc_id:1, path_name:"../picture/male-5.png"},
-    {id: 6, wc_desc_id:14, path_name:"../picture/everyone-6"},
-    {id: 7, wc_desc_id:2, path_name:"../picture/male-7.jpg"},
-    {id: 8, wc_desc_id:13, path_name:"../picture/everyone-8.jpg"},
-    {id: 9, wc_desc_id:13, path_name:"../picture/male-9.jpeg"},
-    {id: 10, wc_desc_id:10, path_name:"../picture/everyone-10.jpg"},
+  await knex('pictures').del()
+  await knex('pictures').insert([
+    {id: 1, wc_desc_id:5, path_name:"../pictures/female-1.png"},
+    {id: 2, wc_desc_id:5, path_name:"../pictures/female-1.png"},
+    {id: 3, wc_desc_id:9, path_name:"../pictures/female-3.jpg"},
+    {id: 4, wc_desc_id:1, path_name:"../pictures/male-4.png"},
+    {id: 5, wc_desc_id:1, path_name:"../pictures/male-5.png"},
+    {id: 6, wc_desc_id:14, path_name:"../pictures/everyone-6"},
+    {id: 7, wc_desc_id:2, path_name:"../pictures/male-7.jpg"},
+    {id: 8, wc_desc_id:13, path_name:"../pictures/everyone-8.jpg"},
+    {id: 9, wc_desc_id:13, path_name:"../pictures/male-9.jpeg"},
+    {id: 10, wc_desc_id:10, path_name:"../pictures/everyone-10.jpg"},
 
   ]);
 };
