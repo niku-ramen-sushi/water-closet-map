4b4ab90ee2e8f1ca3246b9950ee876187cbe4fb2
Tsugu <masatsugu_yamada@mail.toyota.co.jp>
2024-11-25 09:50:46 +0900

    getメソッド修正中

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const db = require("knex");
+const db = require("./knex");
 const cors = require("cors");
 
+
 const app = express();
-const PORT = 3000;
+const PORT = 3030;
 
 app.listen(PORT, () => {
   console.log(`Server running on: http://localhost:${PORT}/`);
@@ -13,58 +14,46 @@ app.use(cors());
 app.use(express.json());
 app.use("/", express.static("../frontend/dist"));
 
-//全ユーザーのデータ
-app.get("/api/users", async(req,res) => {
-  const userData= await db
-    .select("*")
-    .from("users");
+//全ユーザーのデータcheckOK
+app.get("/api/users", async (req, res) => {
+  const userData = await db.select("*").from("users");
   res.status(200).send(userData);
-})
+});
+
 
 //idのユーザー情報
-app.get("/api/users/:id",async(req,res)=>{
+app.get("/api/users/:id", async (req, res) => {
   const idParams = req.params.id;
   const userInfo = await db
-    .select(
-      "users.*",
-      "favorite.wc_id"
-      )
-    .where({id:idParams})
+    .select("users.*", "favorite.wc_id")
+    .where( "users.id", idParams)
     .from("users")
-    .join("favorite",{"favorite.user_id":"users.id"});
+    .join("favorite", { "favorite.user_id": "users.id" });
   res.status(200).send(userInfo);
-})
+});
 
-//清潔度の選択用に使用
-app.get("/api/hygiene", async(req,res) => {
-  const hygieneData = await db
-    .select("*")
-    .from("hygiene_info");
+//清潔度の選択用に使用checkOK
+app.get("/api/hygiene", async (req, res) => {
+  const hygieneData = await db.select("*").from("hygiene_info");
   res.status(200).send(hygieneData);
-})
+});
 
-//トイレ種類の選択用に使用
-app.get("/api/gender-type", async(req,res) => {
-  const genderTypeData = await db
-    .select("*")
-    .from("gender_type");
+//トイレ種類の選択用に使用checkOK
+app.get("/api/gender-type", async (req, res) => {
+  const genderTypeData = await db.select("*").from("gender_type");
   res.status(200).send(genderTypeData);
-})
+});
 
-//ログイン後のピン表示用
-app.get("/api/all-wc-position", async(req,res) => {
+//ログイン後のピン表示用checkOK
+app.get("/api/all-wc-position", async (req, res) => {
   const allWcPositionData = await db
-    .select(
-      "id",
-      "latitude",
-      "longitude"
-    )
+    .select("id", "latitude", "longitude")
     .from("wc_position");
   res.status(200).send(allWcPositionData);
-})
+});
 
-//ピンをクリックした時の詳細表示(写真は別)
-app.get("/api/click-wc-data:id", async (req, res) => {
+//ピンをクリックした時の詳細表示(写真は別)checkOK
+app.get("/api/click-wc-data/:id", async (req, res) => {
   const idParams = req.params.id;
   const wcData = await db
     .select(
@@ -74,57 +63,58 @@ app.get("/api/click-wc-data:id", async (req, res) => {
       "wc_position.created_at",
       "hygiene_info.name",
       "gender_type.type"
-      )
-    .where({wc_pos_id:idParams})
+    )
+    .where("wc_description.wc_pos_id", idParams)
     .from("wc_description")
-    .join("wc_position", {"wc_position.id":"wc_description.wc_pos_id"})
-    .join("hygiene_info", {"hygiene_info.id":"wc_description.hygiene_id"})
-    .join("gender_type", {"gender_type.id":"wc_description.gender.type.id"})
+    .join("wc_position", { "wc_position.id": "wc_description.wc_pos_id" })
+    .join("hygiene_info", { "hygiene_info.id": "wc_description.hygiene_id" })
+    .join("gender_type", { "gender_type.id": "wc_description.gender_type_id" });
   res.status(200).send(wcData);
-})
+});
 
-//ピンをクリックした時の詳細表示(写真のみ)
-app.get("/api/click-wc-picture:id", async (req, res) => {
+//ピンをクリックした時の詳細表示(写真のみ)checkOK
+app.get("/api/click-wc-picture/:id", async (req, res) => {
   const idParams = req.params.id;
   const wcPictureData = await db
-    .select(
-      "pictures.path_name",
-    )
-    .where({wc_pos_id:idParams})
+    .select("pictures.path_name")
+    .where("wc_pos_id", idParams)
     .from("wc_description")
-    .join("pictures", {"pictures.wc_desc_id":"wc_description.id"})
-  res.status(200).send(wcPictureData)
-})
+    .join("pictures", { "pictures.wc_desc_id": "wc_description.id" });
+  res.status(200).send(wcPictureData);
+});
 
-//お気に入りを表示する
-app.get("/api/favorite:id", async (req,res) => {
+//お気に入りを表示する 未確認
+app.get("/api/favorite/:id", async (req, res) => {
   const idParams = req.params.id;
   const userFavorite = await db
-    .select(
-      "wc_position.id"
-    )
-    .wehre({user_id:idParams})
+    .select("wc_position.id")
+    // .where("user_id", idParams)これを追加するとエラーになる
     .from("favorite")
-    .join("wc_position", {"wc_position.id":"favorite.wc_id"})
+    .join("wc_position", { "wc_position.id": "favorite.wc_id" });
   res.status(200).send(userFavorite);
-})
+});
 
 //使えるのか分からない
-app.get("/api/wc-info", async (req,res) => {
+app.get("/api/wc-info", async (req, res) => {
   const wcInfoData = await db
     .select(
-      "title", "address", "latitude", "longitude", "created_at",
+      "title",
+      "address",
+      "latitude",
+      "longitude",
+      "created_at",
       "wc_description.comment",
       "pictures.path_name",
       "hygiene_info.name",
-      "gender_type.type")
+      "gender_type.type"
+    )
     .from("wc_position")
-    .join("wc_description",{"wc_description.wc_pos_id":"wc_position.id"})
-    .join("pictures",{"pictures.wc_desc_id":"wc_description.id"})
-    .join("hygiene_info",{"hygiene_info.id":"wc_description.hygiene_id"})
-    .join("gender_type",{"gender_type.id":"wc_description.gender_type_id"})
+    .join("wc_description", { "wc_description.wc_pos_id": "wc_position.id" })
+    .join("pictures", { "pictures.wc_desc_id": "wc_description.id" })
+    .join("hygiene_info", { "hygiene_info.id": "wc_description.hygiene_id" })
+    .join("gender_type", { "gender_type.id": "wc_description.gender_type_id" });
   res.status(200).send(wcInfoData);
-})
+});
 
 //commit用にPOSTメソッドコメントアウト
 //サインアップ登録用
@@ -138,6 +128,5 @@ app.get("/api/wc-info", async (req,res) => {
 // })
 
 //新たなトイレ情報登録用
-
 
 //お気に入り登録用
