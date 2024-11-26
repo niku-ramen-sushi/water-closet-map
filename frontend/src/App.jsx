467a9e98bbfd26b446eadc7a1014f39a62942ba8
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-27 00:44:08 +0900

    修正：コメントアウト削除

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,3 @@
-// import { Button, HStack, VStack } from '@yamada-ui/react';
-// import DescriptionsForm from './components/forms/DescriptionsForm.jsx';
-// import CreatePinForm from './components/forms/CreatePinForm.jsx';
-// import CreatePinButton from './components/buttons/CreatePinButton.jsx';
-// import { MapPin } from '@yamada-ui/lucide';
-// import { useAtomValue, useSetAtom } from 'jotai';
-// import { isPinEditAtom, pinsAtom, selectedPinIdAtom } from './globalState.js';
 import ToiletMap from './components/pages/ToiletMap.jsx';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/pages/Login.jsx';
@@ -12,10 +5,6 @@ import Notfound from './components/pages/Notfound.jsx';
 import ProtectedRoute from './components/auths/ProtectedRoute.jsx';
 
 function App() {
-  // const pins = useAtomValue(pinsAtom);
-  // const setIsPinEdit = useSetAtom(isPinEditAtom);
-  // const setSelectedPinId = useSetAtom(selectedPinIdAtom);
-
   return (
     <Routes>
       <Route path="/" element={<Login />} />
@@ -29,26 +18,6 @@ function App() {
       />
       <Route path="*" element={<Notfound />} />
     </Routes>
-    // <VStack spacing={2} align="center">
-    //   <CreatePinButton />
-    //   {pins.map((pin, i) => {
-    //     return (
-    //       <HStack spacing={2} align="center" key={i}>
-    //         <MapPin />
-    //         <Button
-    //           onClick={() => {
-    //             setIsPinEdit(true);
-    //             setSelectedPinId(i);
-    //           }}
-    //         >
-    //           {pin.title}
-    //         </Button>
-    //       </HStack>
-    //     );
-    //   })}
-    //   <CreatePinForm />
-    //   <DescriptionsForm />
-    // </VStack>
   );
 }
 
