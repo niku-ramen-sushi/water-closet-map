c36ce79b8b8939eb75635e827b0cb83e0e53e30c
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-28 00:55:03 +0900

    追加：認証ミドルウエアを各エンドポイントへ

--- backend/server.js
+++ backend/server.js
@@ -190,13 +190,13 @@ app.get("/api/auth_check", (req, res) => {
 ///////////////////////////////////////////////////
 
 //全ユーザーのデータcheckOK
-app.get("/api/users", async (req, res) => {
+app.get("/api/users", checkAuth, async (req, res) => {
   const userData = await db.select("*").from("users");
   res.status(200).send(userData);
 });
 
 //idのユーザー情報
-app.get("/api/users/:id", async (req, res) => {
+app.get("/api/users/:id", checkAuth, async (req, res) => {
   const idParams = req.params.id;
   const userInfo = await db
     .select("users.*", "favorite.wc_id")
@@ -207,19 +207,19 @@ app.get("/api/users/:id", async (req, res) => {
 });
 
 //清潔度の選択用に使用checkOK
-app.get("/api/hygiene", async (req, res) => {
+app.get("/api/hygiene", checkAuth, async (req, res) => {
   const hygieneData = await db.select("*").from("hygiene_info");
   res.status(200).send(hygieneData);
 });
 
 //トイレ種類の選択用に使用checkOK
-app.get("/api/gender-type", async (req, res) => {
+app.get("/api/gender-type", checkAuth, async (req, res) => {
   const genderTypeData = await db.select("*").from("gender_type");
   res.status(200).send(genderTypeData);
 });
 
 //ログイン後のピン表示用checkOK
-app.get("/api/all-wc-position", async (req, res) => {
+app.get("/api/all-wc-position", checkAuth, async (req, res) => {
   const allWcPositionData = await db
     .select("id", "latitude", "longitude", "title")
     .from("wc_position");
@@ -235,7 +235,7 @@ app.get("/api/all-wc-position", async (req, res) => {
 //
 // });
 //ピンをクリックした時の詳細表示(写真は別)checkOK-自分の投稿のみ（編集用）//checkAuth,
-app.get("/api/click-wc-data/:id/:userid", async (req, res) => {
+app.get("/api/click-wc-data/:id/:userid", checkAuth, async (req, res) => {
   let { id, userid } = req.params;
   id = Number(id);
   userid = Number(userid);
@@ -262,7 +262,7 @@ app.get("/api/click-wc-data/:id/:userid", async (req, res) => {
 });
 
 //ピンをクリックした時の詳細表示(写真は別)checkOK
-app.get("/api/click-wc-data/:id", async (req, res) => {
+app.get("/api/click-wc-data/:id", checkAuth, async (req, res) => {
   const idParams = req.params.id;
   console.log("----", idParams);
   const wcData = await db
@@ -288,7 +288,7 @@ app.get("/api/click-wc-data/:id", async (req, res) => {
 });
 
 //ピンをクリックした時の詳細表示(写真のみ)checkOK
-app.get("/api/click-wc-picture/:id", async (req, res) => {
+app.get("/api/click-wc-picture/:id", checkAuth, async (req, res) => {
   const idParams = req.params.id;
   const wcPictureData = await db
     .select("pictures.path_name")
@@ -299,7 +299,7 @@ app.get("/api/click-wc-picture/:id", async (req, res) => {
 });
 
 //お気に入りを表示する 未確認
-app.get("/api/favorite/:id", async (req, res) => {
+app.get("/api/favorite/:id", checkAuth, async (req, res) => {
   const idParams = req.params.id;
   const userFavorite = await db
     .select("wc_position.id")
@@ -310,7 +310,7 @@ app.get("/api/favorite/:id", async (req, res) => {
 });
 
 //使えるのか分からない
-app.get("/api/wc-info", async (req, res) => {
+app.get("/api/wc-info", checkAuth, async (req, res) => {
   const wcInfoData = await db
     .select(
       "title",
@@ -343,7 +343,7 @@ app.get("/api/wc-info", async (req, res) => {
 // })
 
 //新たなトイレ情報登録用
-app.post("/api/wc-position", async (req, res) => {
+app.post("/api/wc-position", checkAuth, async (req, res) => {
   const params = req.body;
   const addPosition = await db("wc_position")
     .insert({
@@ -358,7 +358,7 @@ app.post("/api/wc-position", async (req, res) => {
   res.status(201).send(addPosition);
 });
 
-app.post("/api/wc-description", async (req, res) => {
+app.post("/api/wc-description", checkAuth, async (req, res) => {
   const params = req.body;
   console.log(params);
   const addPosition = await db("wc_description")
@@ -375,7 +375,7 @@ app.post("/api/wc-description", async (req, res) => {
 });
 
 //投稿削除
-app.delete("/api/wc-description/:id", async (req, res) => {
+app.delete("/api/wc-description/:id", checkAuth, async (req, res) => {
   const params = req.body;
   console.log(params);
   const addPosition = await db("wc_description")
