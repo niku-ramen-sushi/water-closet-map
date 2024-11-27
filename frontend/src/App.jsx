ccfb73e75708ac2c4fb8853f31ef9e8f0db24f83
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-28 00:37:21 +0900

    Render：app.set("trust proxy", true)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import ToiletMap from './components/pages/ToiletMap.jsx';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/pages/Login.jsx';
 import Notfound from './components/pages/Notfound.jsx';
-// import ProtectedRoute from './components/auths/ProtectedRoute.jsx';
+import ProtectedRoute from './components/auths/ProtectedRoute.jsx';
 import GoogleMap from './components/maps/GoogleMap.jsx';
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
       <Route
         path="/map"
         element={
-          // <ProtectedRoute>
-          <ToiletMap />
-          // </ProtectedRoute>
+          <ProtectedRoute>
+            <ToiletMap />
+          </ProtectedRoute>
         }
       />
       <Route path="/google_map" element={<GoogleMap />} />
